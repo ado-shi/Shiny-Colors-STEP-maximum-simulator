d29037abed71d11352c4eeee066ad90eaf213991
ado-shi <109031310+ado-shi@users.noreply.github.com>
2022-07-11 00:30:21 +0900

    WindowsFormアプリ(というかC#)への置き換え
    - GUI実装
    - 計算ロジックを丸々移植
     - 入力範囲指定は未移植
     - 変数名はC#らしく(？）キャメルケースに変更
     - c++ファイルはporting_sourceに移動

---
+++ ShinyColors_STEP_MaximumSimulator/MainWindow.Designer.cs
@@ -0,0 +1,275 @@
+﻿
+namespace ShinyColors_STEP_MaximumSimulator
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonExecute = new System.Windows.Forms.Button();
+            this.textStatusLimit = new System.Windows.Forms.TextBox();
+            this.textSkillLevel = new System.Windows.Forms.TextBox();
+            this.textSkillPoint = new System.Windows.Forms.TextBox();
+            this.textStatusCount = new System.Windows.Forms.TextBox();
+            this.textStatus = new System.Windows.Forms.TextBox();
+            this.textStatusLimitCount = new System.Windows.Forms.TextBox();
+            this.textResult = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonExecute
+            // 
+            this.buttonExecute.Font = new System.Drawing.Font("メイリオ", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.buttonExecute.Location = new System.Drawing.Point(66, 513);
+            this.buttonExecute.Name = "buttonExecute";
+            this.buttonExecute.Size = new System.Drawing.Size(473, 127);
+            this.buttonExecute.TabIndex = 6;
+            this.buttonExecute.Text = "じっこう";
+            this.buttonExecute.UseVisualStyleBackColor = true;
+            this.buttonExecute.Click += new System.EventHandler(this.ButtonExecuteClick);
+            // 
+            // textStatusLimit
+            // 
+            this.textStatusLimit.Font = new System.Drawing.Font("メイリオ", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textStatusLimit.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.textStatusLimit.Location = new System.Drawing.Point(318, 199);
+            this.textStatusLimit.Name = "textStatusLimit";
+            this.textStatusLimit.Size = new System.Drawing.Size(160, 37);
+            this.textStatusLimit.TabIndex = 1;
+            this.textStatusLimit.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextStatusKeyPress);
+            // 
+            // textSkillLevel
+            // 
+            this.textSkillLevel.Font = new System.Drawing.Font("メイリオ", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textSkillLevel.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.textSkillLevel.Location = new System.Drawing.Point(66, 297);
+            this.textSkillLevel.Name = "textSkillLevel";
+            this.textSkillLevel.Size = new System.Drawing.Size(160, 37);
+            this.textSkillLevel.TabIndex = 2;
+            this.textSkillLevel.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextStatusKeyPress);
+            // 
+            // textSkillPoint
+            // 
+            this.textSkillPoint.Font = new System.Drawing.Font("メイリオ", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textSkillPoint.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.textSkillPoint.Location = new System.Drawing.Point(318, 297);
+            this.textSkillPoint.Name = "textSkillPoint";
+            this.textSkillPoint.Size = new System.Drawing.Size(160, 37);
+            this.textSkillPoint.TabIndex = 3;
+            this.textSkillPoint.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextStatusKeyPress);
+            // 
+            // textStatusCount
+            // 
+            this.textStatusCount.Font = new System.Drawing.Font("メイリオ", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textStatusCount.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.textStatusCount.Location = new System.Drawing.Point(63, 402);
+            this.textStatusCount.Name = "textStatusCount";
+            this.textStatusCount.Size = new System.Drawing.Size(160, 37);
+            this.textStatusCount.TabIndex = 4;
+            this.textStatusCount.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextStatusKeyPress);
+            // 
+            // textStatus
+            // 
+            this.textStatus.Font = new System.Drawing.Font("メイリオ", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textStatus.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.textStatus.Location = new System.Drawing.Point(63, 200);
+            this.textStatus.Name = "textStatus";
+            this.textStatus.Size = new System.Drawing.Size(160, 37);
+            this.textStatus.TabIndex = 0;
+            this.textStatus.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextStatusKeyPress);
+            // 
+            // textStatusLimitCount
+            // 
+            this.textStatusLimitCount.Font = new System.Drawing.Font("メイリオ", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textStatusLimitCount.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.textStatusLimitCount.Location = new System.Drawing.Point(318, 401);
+            this.textStatusLimitCount.Name = "textStatusLimitCount";
+            this.textStatusLimitCount.Size = new System.Drawing.Size(160, 37);
+            this.textStatusLimitCount.TabIndex = 5;
+            this.textStatusLimitCount.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextStatusKeyPress);
+            // 
+            // textResult
+            // 
+            this.textResult.Font = new System.Drawing.Font("メイリオ", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textResult.Location = new System.Drawing.Point(652, 193);
+            this.textResult.Multiline = true;
+            this.textResult.Name = "textResult";
+            this.textResult.ReadOnly = true;
+            this.textResult.Size = new System.Drawing.Size(439, 454);
+            this.textResult.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("メイリオ", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(39, 165);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(173, 30);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "特化ステータス値";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("メイリオ", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(303, 165);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(193, 30);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "特化ステータス上限";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("メイリオ", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.Location = new System.Drawing.Point(39, 264);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(233, 30);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "特化ステータスの熟練度";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("メイリオ", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label4.Location = new System.Drawing.Point(303, 264);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 30);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "団結力";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("メイリオ", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label5.Location = new System.Drawing.Point(39, 368);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(253, 30);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "特化ステータスの成長回数";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("メイリオ", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label6.Location = new System.Drawing.Point(303, 368);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(293, 30);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "特化ステータス上限の成長回数";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("メイリオ", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label7.Location = new System.Drawing.Point(634, 158);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(73, 30);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "けっか";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("メイリオ", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label8.Location = new System.Drawing.Point(112, 35);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(582, 36);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "6個のパラメータを入力して「じっこう」を押そう！";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("メイリオ", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label9.ForeColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label9.Location = new System.Drawing.Point(980, 769);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(126, 24);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "誰かUI直して...";
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1138, 818);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textResult);
+            this.Controls.Add(this.textStatusLimitCount);
+            this.Controls.Add(this.textStatus);
+            this.Controls.Add(this.textStatusCount);
+            this.Controls.Add(this.textSkillPoint);
+            this.Controls.Add(this.textSkillLevel);
+            this.Controls.Add(this.textStatusLimit);
+            this.Controls.Add(this.buttonExecute);
+            this.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.Name = "MainWindow";
+            this.Text = "Shiny-Colors-STEP-maximum-simulator";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextStatusKeyPress);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonExecute;
+        private System.Windows.Forms.TextBox textStatusLimit;
+        private System.Windows.Forms.TextBox textSkillLevel;
+        private System.Windows.Forms.TextBox textSkillPoint;
+        private System.Windows.Forms.TextBox textStatusCount;
+        protected System.Windows.Forms.TextBox textStatus;
+        private System.Windows.Forms.TextBox textStatusLimitCount;
+        private System.Windows.Forms.TextBox textResult;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ ShinyColors_STEP_MaximumSimulator/MainWindow.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ShinyColors_STEP_MaximumSimulator
+{
+    public partial class MainWindow : Form
+    {
+        private int status;
+        private int statusLimit;
+        private int skillLevel;
+        private int skillPoint;
+        private int statusCount;
+        private int statusLimitCount;
+
+		//-------------------------------
+		//  Instance 
+		//-------------------------------
+
+		public MainWindow()
+        {
+            InitializeComponent();
+			
+        }
+
+
+		//-------------------------------
+		//  EventHandler 
+		//-------------------------------
+
+		/// <summary>
+		/// じっこうボタン押下
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void ButtonExecuteClick(object sender, EventArgs e)
+        {
+            try {
+                status = int.Parse(textStatus.Text);
+                statusLimit = int.Parse(textStatusLimit.Text);
+                skillLevel = int.Parse(textSkillLevel.Text);
+                skillPoint = int.Parse(textSkillPoint.Text);
+                statusCount = int.Parse(textStatusCount.Text);
+                statusLimitCount = int.Parse(textStatusLimitCount.Text);
+            }
+            catch (Exception) {
+                // 無いと思うけど。。。
+                textResult.Text = "入力パラメータが不正です。";
+
+            }
+            textResult.Text = "計算中";
+
+			// ステータス計算実行
+			ExecuteCalc();
+        }
+
+		/// <summary>
+		/// ステータス入力項目制限
+		///  TextBoxへの数値以外の入力を制限
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+        private void TextStatusKeyPress(object sender, KeyPressEventArgs e)
+        {
+            //バックスペースが押された時は有効（Deleteキーも有効）
+            if (e.KeyChar == '\b')
+            {
+                return;
+            }
+
+            //数値0～9以外が押された時はイベントをキャンセルする
+            if ((e.KeyChar < '0' || '9' < e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+
+		//-------------------------------
+		//  Internals
+		//-------------------------------
+		
+		/// <summary>
+		/// ステータス計算
+		/// </summary>
+		private void ExecuteCalc() 
+        {
+			int requiredStatusPoint = 0;              //ステータスの次の成長に必要な熟練度(歌唱力or安定感or表現力or集中力)
+			int requiredLimitPoint = 0;               //ステータス上限の次の成長に必要な熟練度(歌唱力or安定感or表現力or集中力)
+			int requiredStatusSkillPoint = 0;         //ステータスの次の成長に必要な団結力
+			int requiredLimitSkillPoint = 0;        　//ステータス上限の次の成長に必要な団結力
+			int initialSkillPoint = skillPoint;       //団結力の初期値
+
+            /* ToDo:値の入力制限を実装 */
+
+            //ステータスが成長できるorステータス上限値が成長できる場合実行
+            while ((skillLevel - requiredStatusPoint >= 0 && skillPoint - requiredStatusSkillPoint >= 0)                        
+				|| (skillLevel - requiredLimitPoint >= 0 && skillPoint - requiredLimitSkillPoint >= 0) && (statusCount < 230))　//
+			{
+                //ステータスとステータス上限値の次の成長に必要な必要熟練度pt及び必要団結力を求める
+                RequiredPoint(statusCount, requiredStatusPoint, requiredStatusSkillPoint, ref statusCount, ref requiredStatusPoint, ref requiredStatusSkillPoint);
+                RequiredPoint(statusLimitCount, requiredLimitPoint, requiredLimitSkillPoint, ref statusLimitCount, ref requiredLimitPoint, ref requiredLimitSkillPoint);
+
+                if (((status + 10) <= statusLimit)                    //特化ステータスが上限値-10の値で成長できる
+                    && ((skillLevel - requiredStatusPoint) >= 0)      //かつ、特化ステータスの成長に必要な熟練度ptを保持している
+                    && ((skillPoint - requiredStatusSkillPoint) >= 0) //かつ、特化ステータスの成長に必要な団結力ptを保持している
+                    && (statusCount < 230))                           //かつ、特化ステータスの成長回数が230回(カンスト)未満の場合、実行
+                {                                       
+                    status = status + 10;
+                    skillLevel = skillLevel - requiredStatusPoint;
+                    skillPoint = skillPoint - requiredStatusSkillPoint;
+                    statusCount++;
+                }
+                else if (((skillLevel - requiredLimitPoint) >= 0)     //上のif文が実行されず、特化ステータス上限値の成長に必要な熟練度ptを保持している
+                    && ((statusLimit - status) <= 10)                 //かつ、現在の特化ステータスと上限値の差が10以下
+                    && ((skillLevel - 2 * requiredStatusPoint) >= 0)  //かつ、熟練度が2回の成長で枯渇しない
+                    && ((skillPoint - requiredLimitSkillPoint) >= 0)  //かつ、特化ステータス上限値の成長に必要な団結力ptを保持している
+                    && (statusLimitCount < 140))                      //かつ、特化ステータス上限の成長回数が140回(カンスト)未満の場合、実行
+                {
+                    statusLimit = statusLimit + 10;
+                    skillLevel = skillLevel - requiredLimitPoint;
+                    skillPoint = skillPoint - requiredLimitSkillPoint;
+                    statusLimitCount++;
+
+                }
+                else if ((status < statusLimit)                       //上記のif文、else if文が実行されず、現在の特化ステータス値が上限値以下
+                    && ((skillLevel - requiredStatusPoint) >= 0)      //特化ステータスの成長に必要な熟練度ptを保持している
+                    && ((skillPoint - requiredStatusSkillPoint) >= 0) //かつ、特化ステータスの成長に必要な団結力ptを保持している
+                    && (statusCount < 230))                           //かつ、特化ステータスの成長回数が230回(カンスト)未満の場合、実行
+                {                                       
+                    status = statusLimit;
+                    skillLevel = skillLevel - requiredStatusPoint;
+                    skillPoint = skillPoint - requiredStatusSkillPoint;
+                    statusCount++;
+                }
+                else
+                {
+                    // 計算終了
+                    break;
+                }
+
+            }
+
+            // 計算結果表示
+            textResult.Text = "最終的な最大ステータス情報は\r\n";
+            textResult.AppendText(String.Format("特化ステータス:{0}\r\n", status));
+            textResult.AppendText(String.Format("特化上限:{0}\r\n", statusLimit));
+            textResult.AppendText(String.Format("累計特化ステータス成長回数:{0}\r\n", statusCount));
+            textResult.AppendText(String.Format("累計特化上限成長回数:{0}\r\n", statusLimitCount));
+            textResult.AppendText(String.Format("残り熟練度:{0}\r\n", skillLevel));
+            textResult.AppendText(String.Format("残り団結力:{0}\r\n\r\n", skillPoint));
+
+            textResult.AppendText(String.Format("SP変換可能回数:{0}\r\n", skillPoint / 30));
+            textResult.AppendText(String.Format("SP変換値:{0}\r\n", skillPoint / 30 * 10));
+            textResult.AppendText(String.Format("SP変換後残り団結力:{0}\r\n", initialSkillPoint - 30 * (skillPoint / 30)));
+            textResult.AppendText(String.Format("上記を目安にSPに変換、上限パネルを解放した後もう一度実行してください\r\n"));
+
+        }
+
+        /// <summary>
+        /// 特化ステータスor上限の次の成長に必要な熟練度と団結力の値を返す関数
+        /// </summary>
+        /// <param name="count"></param>
+        /// <param name="point"></param>
+        /// <param name="danketsuryoku_point"></param>
+        /// <param name="p_count"></param>
+        /// <param name="p_point"></param>
+        /// <param name="p_danketsuryoku_point"></param>
+        /// <returns></returns>
+        private static int RequiredPoint(int count, int point, int danketsuryoku_point, ref int p_count, ref int p_point, ref int p_danketsuryoku_point)
+		{
+			if (0 <= count && count < 30)
+			{
+				point = 10;
+				danketsuryoku_point = 0;
+			}
+			else if (30 <= count && count < 60)
+			{
+				point = 15;
+				danketsuryoku_point = 0;
+			}
+			else if (60 <= count && count < 90)
+			{
+				point = 20;
+				danketsuryoku_point = 1;
+			}
+			else if (90 <= count && count < 120)
+			{
+				point = 30;
+				danketsuryoku_point = 2;
+			}
+			else if (120 <= count && count < 150)
+			{
+				point = 40;
+				danketsuryoku_point = 3;
+			}
+			else if (150 <= count && count < 230)
+			{
+				point = 50;
+				danketsuryoku_point = 4;
+			}
+			p_count = count;
+			p_point = point;
+			p_danketsuryoku_point = danketsuryoku_point;
+			return 0;
+		}
+
+    }
+
+}
